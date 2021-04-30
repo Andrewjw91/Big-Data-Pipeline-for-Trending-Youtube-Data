bd2860601688414df06cd51086a2de9beb464c93
Andrewjw91 <80496873+Andrewjw91@users.noreply.github.com>
2021-04-30 02:50:53 -0700

    Add files via upload

--- code/youtube.py
+++ code/youtube.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime,date
 import seaborn as sns
 from sqlalchemy import create_engine
 engine = create_engine("sqlite:///youtube.db")
@@ -10,10 +11,28 @@ engine = create_engine("sqlite:///youtube.db")
 
 st.title('YouTube Trending Video Data By Country')
 
+#SideBar
 st.sidebar.header('Filters')
+#Country Select
 country = st.sidebar.selectbox('Pick a country to show some info about',('BR', 'CA', 'DE', 'FR', 'GB', 'IND', 'JP', 'KR', 'MX', 'RUS', 'US'))
+#Category Select
 category = st.sidebar.selectbox('Pick a Video Category', ('ALL', 'Autos & Vehicles', 'Comedy', 'Education', 'Entertainment', 'Film and Animation', 'Gaming', 'Howto & Style', 'Music', 'News & Politics', 'Nonprofits & Activism', 'People & Blogs', 'Pets & Animals', 'Science & Technology', 'Sports', 'Travel & Events'))
+#
+st.sidebar.markdown('''---''')
+#Date Select
+st.sidebar.header('Pick a date range')
+date_data = pd.read_sql('SELECT * FROM {}'.format(country), engine)
+df = date_data
+df['trending_date'] = pd.to_datetime(df['trending_date'])
 
+
+start_date = st.sidebar.date_input(label='Start date', min_value= df['trending_date'].min(), max_value= df['trending_date'].max(), value= df['trending_date'].min())
+end_date = st.sidebar.date_input(label='End date', min_value= df['trending_date'].min(), max_value= df['trending_date'].max(), value= df['trending_date'].max())
+if start_date > end_date:
+    st.error('Error: End date must fall after start date.')
+
+
+#Category: ID Key Mapping
 cat_dict = {'Autos & Vehicles': 2, 'Comedy': 23, 'Education': 27,
 'Entertainment': 24, 'Film and Animation': 1, 'Gaming': 20,
 'Howto & Style': 26, 'Music': 10, 'News & Politics': 25,
@@ -45,7 +64,7 @@ button2 = st.button('Top Channels By Category')
 if button2:
     if category == 'ALL':
         st.write('## Top Trending Channels (ALL Categories)')
-        frequent_channels = pd.read_sql('''SELECT channelTitle, COUNT(channelTitle) FROM {}
+        frequent_channels = pd.read_sql('''SELECT channelTitle, COUNT(channelTitle), trending_date FROM {0}
             GROUP BY channelTitle
             ORDER BY COUNT(channelTitle)
             DESC LIMIT 15'''.format(country), engine)
@@ -68,20 +87,7 @@ if button2:
         st.pyplot(fig)
 #
 
-st.markdown('''---''')
-date_data = pd.read_sql('SELECT * FROM {}'.format(country), engine)
-df = date_data
-df['trending_date'] = pd.to_datetime(df['trending_date'])
-date = st.date_input(label='Choose a date', min_value= df['trending_date'].min(), max_value= df['trending_date'].max())
-button3 = st.button('Show Trending Videos By Date')
 
-
-#-----
-
-
-
-if button3:
-    st.dataframe(df[df['trending_date']== date])
 
 
 
